17864319f0333902bf069eb2cc8399926b11779e
Siroson Sivarajah <siroson.sivarajah@tvtail.com>
2020-08-25 16:14:00 -0400

    Created files and folders architecture

---
+++ ContactsApp/Screens/ContactPermission/ContactPermissionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactPermissionViewController.swift
+//  ContactsApp
+//
+//  Created by Siroson on 2020-08-25.
+//  Copyright © 2020 Siroson. All rights reserved.
+//
+
+import UIKit
+
+class ContactPermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ContactsApp/Screens/ContactsList/ContactsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsListViewController.swift
+//  ContactsApp
+//
+//  Created by Siroson on 2020-08-25.
+//  Copyright © 2020 Siroson. All rights reserved.
+//
+
+import UIKit
+
+class ContactsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
