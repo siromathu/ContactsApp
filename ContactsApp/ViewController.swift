009ba00fa86d60a5c2e50fed8daf3229c4756798
Siroson Sivarajah <siroson.sivarajah@tvtail.com>
2020-08-25 16:08:37 -0400

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Siroson on 2020-08-25.
+//  Copyright © 2020 Siroson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
