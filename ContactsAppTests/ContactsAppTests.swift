e7da02aa84592190b3654a6f599eafc66723b8c8
Siroson Sivarajah <siroson.sivarajah@tvtail.com>
2020-08-27 16:44:07 -0400

    Sample test cases are added to test grouping of contacts based on alphabets

--- ContactsAppTests/ContactsAppTests.swift
+++ ContactsAppTests/ContactsAppTests.swift
@@ -10,25 +10,40 @@ import XCTest
 @testable import ContactsApp
 
 class ContactsAppTests: XCTestCase {
+    
+    var contactsListController: ContactsListViewController!
+    
+    var contactDetailController: ContactDetailViewController!
+    
+    let contactOne = ContactDetail(id: UUID().uuidString, firstName: "Matt", lastName: "Damon", numbers: [ContactItem(label: "mobile", value: "1111111111")])
+    let contactTwo = ContactDetail(id: UUID().uuidString, firstName: "Mark", lastName: "Zuckerberg", numbers: [ContactItem(label: "mobile", value: "2222222222")])
+    let contactThree = ContactDetail(id: UUID().uuidString, firstName: "Jeff", lastName: "Bezos", numbers: [ContactItem(label: "mobile", value: "3333333333")])
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        contactsListController = ContactsListViewController()
+        contactsListController.allContacts.append(contentsOf: [contactOne, contactTwo, contactThree])
+        
+        contactDetailController = ContactDetailViewController()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        contactsListController.allContacts.removeAll()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    // Sample test contacts grouping test cases
+    
+    func testContactsGroupingFails() {
+        let expectedGroups = [ContactGroup(title: "J", contacts: [contactThree])]
+        let actualGroups = ContactsManager.sort(contacts: contactsListController.allContacts)
+        XCTAssertNotEqual(actualGroups, expectedGroups, "Grouping failed as expected")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testContactsGroupingPasses() {
+        let groupOne = ContactGroup(title: "J", contacts: [contactThree])
+        let groupTwo = ContactGroup(title: "M", contacts: [contactTwo, contactOne])
+        let expectedGroups = [groupOne, groupTwo]
+        let actualGroups = ContactsManager.sort(contacts: contactsListController.allContacts)
+        XCTAssertEqual(actualGroups, expectedGroups, "Grouping failed")
     }
-
 }
